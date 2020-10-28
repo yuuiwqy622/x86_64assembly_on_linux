5801d184cb246181f0dcd857e106f3317d7b8ad2
yuuiwqy622 <random064@gmail.com>
2020-10-28 18:23:40 +0100

    Print number to standard output using SYS_WRITE

---
+++ print_num.asm
@@ -0,0 +1,55 @@
+;program prints number to standard output
+;using SYS_WRITE
+
+;system calls
+SYS_WRITE equ 1
+SYS_EXIT equ 60
+
+;file descriptors
+STDOUT equ 1
+
+;number to print
+NUM equ 123
+
+section .bss
+  digit resb 100
+  dPos resb 8
+
+section .text
+  global start
+
+start:
+  mov rax,NUM
+  call _printNum
+
+  mov rax,SYS_EXIT
+  mov rdi,0
+  syscall
+
+_printNum:
+  mov rbx,10
+  mov [digit],rbx
+  mov rsi,digit+1
+
+readDigit:
+  mov rdx,0
+  div rbx
+  add rdx,'0'
+
+  mov [rsi],dl
+  inc rsi
+  cmp rax,0
+  jne readDigit
+
+  mov rax,SYS_WRITE
+  mov rdi,STDOUT
+  mov rdx,1
+
+printDigit:
+  syscall
+
+  dec rsi
+  cmp rsi,digit
+  jge printDigit
+
+  ret
